b6e4d5df44ff77ef7513d3ecb60c364eb8a5e9ba
KerwinAngeles <kerwinangeles12@gmail.com>
2025-05-16 03:14:09 +0200

    init

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Header } from './components/header'
 import UserInput from './components/UserInput'
 
 const App = () => {
-  return (
+  return (  
     <main className='min-h-screen mx-auto py-20 bg-gradient-to-t gap-20 from-green-300 to-green-600'>
        <Header/>
        <UserInput/>
